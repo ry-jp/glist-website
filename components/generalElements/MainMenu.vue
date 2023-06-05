0e1bef376cf398bee2ae47ce680b88cb900ff731
ry-jp <ry_design@protonmail.com>
2023-06-05 16:02:43 -0500

    colors and menu update

--- components/generalElements/MainMenu.vue
+++ components/generalElements/MainMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <NuxtLink to="/">{{ $t('messages.general.home') }}</NuxtLink>
-    <NuxtLink to="/products">{{ $t('messages.general.allProducts') }}</NuxtLink>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/products">Clothes</NuxtLink>
     <NuxtLink to="/categories">{{ $t('messages.shop.category', 2) }}</NuxtLink>
     <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
   </nav>
